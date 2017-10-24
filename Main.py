7527b987590d3df691384342416e766bd91d3c7a
arikarchmer <ari.karchmer.s@gmail.com>
2017-10-24 13:12:59 -0400

    added thompson sampler

--- Main.py
+++ Main.py
@@ -2,31 +2,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 from MAB.MAB import MAB
 from Arm.Arm import Arm
+from Thompson.ThompsonSampler import ThompsonSampler as ts
 
 if __name__ =="__main__":
 
-    a1 = Arm(0.3, 0)
-    a2 = Arm(0.87, 1)
-    a3 = Arm(0.26, 2)
-    a4 = Arm(0.65, 3)
-    a5 = Arm(.65, 4)
+    a1 = Arm(0.5, 0)
+    a2 = Arm(0.505, 1)
+    a3 = Arm(0.503, 2)
+    a4 = Arm(0.499, 3)
+    a5 = Arm(.497, 4)
+
+    optimal = 0.505
 
     arms = [a1, a2, a3, a4, a5]
     n_arms = len(arms)
     mab = MAB([1.0/n_arms for a in arms], [[0, 0] for a in arms])
 
+    E = []
     res = []
     means = []
-    for itr in range(2500):
-        chosen = mab.choose_arm(arms)
-        reward = chosen.select()
+    choices = []
+    p = {0: 0.5, 1: 0.5, 2: 0.5, 3: 0.5, 4: 0.5}
+    sampler = ts(0)
+
+    for itr in range(1000):
+        nE = len(E)
+        E_prime = []
+        chosen = sampler.sample(E, E_prime, nE, p, n_arms, arms)
+
+        reward = arms[chosen].select()
+        E.append([chosen, reward])
         res.append(reward)
         means.append(float(sum(res))/len(res))
-        mab.recalibrate(reward, arms, chosen)
+        choices.append([sum(l) for l in mab.observations])
+        mab.recalibrate(reward, arms, arms[chosen])
     
     print mab.distribution
     print mab.observations
 
-    plt.plot(means)
+    print 
+    plt.plot([[optimal,m] for m in means])
+    plt.show()
+    plt.plot(choices)
     plt.show()
             
---
+++ Thompson/ThompsonSampler.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+class ThompsonSampler():
+
+    def __init__(self, n):
+        self.n = n
+
+    def sample(self, E, E_prime, nE, p, n_arms, arms):
+        
+        if nE > 0:
+            d = {}
+            for i in range(nE):
+                E_prime.append(E[np.random.randint(nE)])
+            
+            for item in E_prime:
+                if item[0] not in d:
+                    d[item[0]] = [item[1]]
+                else:
+                    d[item[0]].append(item[1])
+            for k, v in d.iteritems():
+                p[k] = float(sum(v))/len(v)
+
+            print sorted(p.iteritems(), key=lambda (k,v): (v,k))
+            chosen = sorted(p.iteritems(), key=lambda (k,v): (v,k))[len(p) - 1][0]
+
+        else:
+            chosen = arms[np.random.randint(n_arms)].index
+
+        return chosen
